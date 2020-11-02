b81aff4ab3d961ef4bc635105ff790084f768cdc
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-11-02 15:56:42 -0600

    edit to queries

--- queries.sql
+++ queries.sql
@@ -8,6 +8,9 @@ select * from suppliers where supplierid = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
 ?????
+-- //SELECT * FROM Orders order by OrderDate desc limit 10 this is the solution code- not right!
+
+
 --//first 10 ascending order !! SELECT * FROM orders ORDER BY orderdate LIMIT 10;
 -- //last 10 orders!! SELECT * FROM orders ORDER BY orderdate DESC LIMIT 10;
 
@@ -18,8 +21,7 @@ SELECT * FROM customers WHERE Country='Brazil' or City ='London' or City = 'Madr
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('The Shire','Bilbo Baggins','1 Hobbit-Hole','Bag End','111','Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-?????
-
+update Customers set PostalCode = '11122' where CustomerID = 92
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
